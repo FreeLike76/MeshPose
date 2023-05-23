9dc02fddf752676cb0c44cf981ac3f1faef37abc
Dmytro Heleshko <dmytro.geleshko01@gmail.com>
2023-05-23 22:01:11 +0300

    decrease n features

--- run_eval.py
+++ run_eval.py
@@ -59,7 +59,7 @@ def main(data_p: Path, verbosity: int = False):
         #"ROOT_SIFT": extractors.ClassicalFeatureExtractor(detector="SIFT", descriptor="ROOT_SIFT", verbosity=1),
         #"GFTT_SIFT": extractors.ClassicalFeatureExtractor(detector="GFTT", descriptor="SIFT", verbosity=1),
         # SilkFeatureExtractor
-        "SILK": extractors.SilkFeatureExtractor(checkpoints_p=Path("checkpoints/coco-rgb-aug.ckpt"), device="cuda:0", verbosity=verbosity),
+        "SILK": extractors.SilkFeatureExtractor(checkpoints_p=Path("checkpoints/coco-rgb-aug.ckpt"), device="cuda:0", top_k=300, verbosity=verbosity),
     }
     
     # Preprocess dataset
