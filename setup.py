e7449f30bba3ab71d6fe031d03de3fb652474aaa
Dmytro Heleshko <dmytro.geleshko01@gmail.com>
2023-05-31 20:49:06 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mpose",
+    version="0.1.0",    
+    description="Flexible Feature-based Visual Localization Library.",
+    url="https://github.com/FreeLike76/CameraLocalization",
+    author="Dmytro Heleshko",
+    author_email="dmytro.geleshko01@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+)
